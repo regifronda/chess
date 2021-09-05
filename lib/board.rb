fe0c1982281f651b6b04ecddf164e7ef3c851e67
Regi Fronda <regisfronda@gmail.com>
2021-09-05 00:21:26 -0700

    Add render method

--- lib/board.rb
+++ lib/board.rb
@@ -4,4 +4,15 @@ class Board
   def initialize
     @board = Array.new(8) { Array.new(8) }
   end
+
+  def render
+    puts
+    @board.each do |row|
+      row.each do |cell|
+      cell.nil? ? print("-") : print(cell.to_s)
+      end
+      puts
+    end
+    puts
+  end
 end
