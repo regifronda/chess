f42f7dd4dfcd3ed9d039b4eb0febb25e5ae62101
Regi Fronda <regisfronda@gmail.com>
2021-09-09 02:00:17 -0700

    Add move_directions

---
+++ lib/knight.rb
@@ -0,0 +1,15 @@
+class Knight
+  attr_reader :color
+  
+  def initialize(color)
+    @color = color
+  end
+
+  def move_directions
+    [[1, 2], [2, 1], [-1, 2], [-2, 1], [1, -2], [2, -1], [-1, -2], [-2, -1]]
+  end
+
+  def to_s
+    color == :black ? "♞" : "♘"
+  end
+end
