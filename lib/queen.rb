0396c5cc24be0f6544a492465f960e2cdc5d23c1
Regi Fronda <regisfronda@gmail.com>
2021-09-10 03:37:38 -0700

    Add move_directions  method

--- lib/queen.rb
+++ lib/queen.rb
@@ -5,6 +5,11 @@ class Bishop
     @color = color
   end
 
+  def move_directions
+    [
+      [0, 1], [1, 1], [1, 0], [0, -1], [1, -1], [-1, 1], [-1, -1], [-1, 0]
+    ]
+  end
   def to_s
     color == :black ? "♝" : "♗"
   end
