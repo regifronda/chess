8c9afa71241ab7071fd90de8b18543663b9a38c4
Regi Fronda <regisfronda@gmail.com>
2021-09-25 21:01:12 -0700

    Remove require_relative lines

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,3 @@
-require_relative './pawn.rb'
-require_relative './rook.rb'
-require_relative './knight.rb'
-require_relative './bishop.rb'
-require_relative './queen.rb'
-require_relative './king.rb'
-
 class Board
   attr_accessor :grid
   
