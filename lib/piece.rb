e28a53205380ec66c0455ec4b22fd8553009261f
Regi Fronda <regisfronda@gmail.com>
2021-09-25 21:52:08 -0700

    Add enemy? method

--- lib/piece.rb
+++ lib/piece.rb
@@ -2,6 +2,11 @@ class Piece
   attr_reader :color, :board
   
   def initialize(board, color)
+    @board = board
     @color = color
+  end
+
+  def enemy?(location)
+    !board[location].nil? && board[location].color != color
   end
 end
