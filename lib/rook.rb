dd8763705723af2bac19be7624f0d17cd96b8230
Regi Fronda <regisfronda@gmail.com>
2021-09-27 22:36:29 -0700

    Add available_moves method

--- lib/rook.rb
+++ lib/rook.rb
@@ -5,6 +5,33 @@ class Rook < Piece
     ]
   end
 
+  def available_moves
+    moves = []
+
+    move_directions.each do |(row_difference, column_difference)|
+      current_row, current_column = location
+      
+      loop do
+        current_row += row_difference
+        current_column += column_difference
+        loc = [current_row, current_column]
+        break if !board.in_bounds?(loc)
+
+        if board.empty?(loc)
+          moves << loc
+        end
+        
+        if enemy?(loc)
+          moves << loc
+          break
+        end
+      end
+      # until we hit a piece
+        # add the location + difference in column/row to moves
+    end
+    moves
+  end
+
   def to_s
     color == :black ? "♜" : "♖"
   end
