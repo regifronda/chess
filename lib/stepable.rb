9af51c7a7f13ae575fb7b0c2d5524a880a20221b
Regi Fronda <regisfronda@gmail.com>
2021-09-29 23:53:04 -0700

    Add changes so available_moves reflects movement for pieces like king, pawn, and knight

--- lib/stepable.rb
+++ lib/stepable.rb
@@ -5,21 +5,14 @@ module Stepable
     move_directions.each do |(row_difference, column_difference)|
       current_row, current_column = location
       
-      loop do
-        current_row += row_difference
-        current_column += column_difference
-        loc = [current_row, current_column]
-        break if !board.in_bounds?(loc)
-
-        if board.empty?(loc)
-          moves << loc
-        end
-
-        if enemy?(loc)
-          moves << loc
-          break
-        end
-      end
+      current_row += row_difference
+      current_column += column_difference
+      loc = [current_row, current_column]
+      next if !board.in_bounds?(loc)
+      
+      if board.empty?(loc) || enemy?(loc)
+        moves << loc
+      end     
     end
     moves
   end
