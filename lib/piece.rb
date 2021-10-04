e580d547742d0411c780a271ade90ab49ce2ac5b
Regi Fronda <regisfronda@gmail.com>
2021-10-04 15:23:15 -0700

    Add current_row and current_column methods

--- lib/piece.rb
+++ lib/piece.rb
@@ -10,4 +10,12 @@ class Piece
   def enemy?(location)
     !board[location].nil? && board[location].color != color
   end
+
+  def current_row
+    location.first
+  end
+
+  def current_column
+    location.last
+  end
 end
