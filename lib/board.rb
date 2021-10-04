5fb53787102be1669b866359da8b50d1815a542d
Regi Fronda <regisfronda@gmail.com>
2021-10-03 17:54:10 -0700

    Add changes to self.start_chess so that the pieces take in the instance of the board instead of the class board

--- lib/board.rb
+++ lib/board.rb
@@ -4,24 +4,24 @@ class Board
   def self.start_chess
     board = self.new
     8.times do |column|
-      board[[1, column]] = Pawn.new(self, [1, column], :black)
-      board[[6, column]] = Pawn.new(self, [6, column],:white)
+      board[[1, column]] = Pawn.new(board, [1, column], :black)
+      board[[6, column]] = Pawn.new(board, [6, column],:white)
     end
     [[0, :black], [7, :white]].each do |(row, color)|
-      board[[row, 0]] = Rook.new(self, [row, 0], color)
-      board[[row, 7]] = Rook.new(self, [row, 7], color)
+      board[[row, 0]] = Rook.new(board, [row, 0], color)
+      board[[row, 7]] = Rook.new(board, [row, 7], color)
 
-      board[[row, 1]] = Knight.new(self, [row, 1], color)
-      board[[row, 6]] = Knight.new(self, [row, 6], color)
+      board[[row, 1]] = Knight.new(board, [row, 1], color)
+      board[[row, 6]] = Knight.new(board, [row, 6], color)
 
-      board[[row, 2]] = Bishop.new(self, [row, 2], color)
-      board[[row, 5]] = Bishop.new(self, [row, 5], color)
+      board[[row, 2]] = Bishop.new(board, [row, 2], color)
+      board[[row, 5]] = Bishop.new(board, [row, 5], color)
     end
-    board[[0, 3]] = King.new(self, [0, 3], :black)
-    board[[0, 4]] = Queen.new(self, [0, 4], :black)
+    board[[0, 3]] = King.new(board, [0, 3], :black)
+    board[[0, 4]] = Queen.new(board, [0, 4], :black)
 
-    board[[7, 3]] = King.new(self, [7, 3], :white)
-    board[[7, 4]] = Queen.new(self, [7, 4], :white)
+    board[[7, 3]] = King.new(board, [7, 3], :white)
+    board[[7, 4]] = Queen.new(board, [7, 4], :white)
 
     board
   end
