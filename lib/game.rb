37035cc88dd90b53f6ad2518a1bcdbe8aa7a0092
Regi Fronda <regisfronda@gmail.com>
2021-10-14 13:08:40 -0700

    Add play method so user selects a piece and position to move to

--- lib/game.rb
+++ lib/game.rb
@@ -2,9 +2,27 @@ class Game
   attr_reader :player1, :player2
   attr_accessor :current_player
 
-  def initialize 
+  def initialize
     @player1 = Player.new(:black)
     @player2 = Player.new(:white)
     @current_player = @player1
+  end
+
+  def swap_player!
+    self.current_player = (current_player == player1 ? player2 : player1)
+  end
+
+  def play
+    puts "It's #{current_player.color}'s turn"
+    
+    loop do
+    puts "Select a piece: "
+    start_position = current_player.get_position
+    p start_position
+    end
+
+    puts "Select a position to move to:"
+    end_position = current_player.get_position
+    p end_position
   end
 end
