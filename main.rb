9f9dfaa4283379e6758e722764c72e00356b3ecf
Regi Fronda <regisfronda@gmail.com>
2021-10-14 00:34:59 -0700

    Add require_relative for lib/game.rb

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require_relative './lib/board.rb'
 require_relative './lib/board_renderer_text.rb'
 require_relative './lib/pieces.rb'
 require_relative './lib/player.rb'
+require_relative './lib/game.rb'
 
 
 # board = Board.new
