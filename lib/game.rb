ad8c1b68e5b777a99d0b6a5f9fd89fe8eb3694fb
Regi Fronda <regisfronda@gmail.com>
2021-10-17 00:27:30 -0700

    Add take_turn method

--- lib/game.rb
+++ lib/game.rb
@@ -2,10 +2,9 @@ class Game
   attr_reader :player1, :player2, :board, :renderer
   attr_accessor :current_player
 
-  def initialize(board, player1, player2, renderer)
+  def initialize(board, player1, player2, renderer_class)
     @board = board
-    @renderer = renderer
-    renderer.board = board
+    @renderer = renderer_class.new(board)
     @player1 = player1
     @player2 = player2
     @current_player = @player1
@@ -16,20 +15,36 @@ class Game
   end
 
   def play
+    renderer.render
     puts "It's #{current_player.color}'s turn"
-    
+    take_turn
+    swap_player!
+  end
+
+  def take_turn
+    start_position = nil
+
     loop do
-    puts "Select a piece to move: "
-    start_position = current_player.get_position
-    if board[start_position].color == current_player.color
-      break
-    end
-    puts "Did not select a #{current_player.color} piece"
-    p start_position
+      puts "Select a piece to move: "
+      start_position = current_player.get_position
+      if !board[start_position].nil? && 
+      board[start_position].color == current_player.color
+        break
+      end
+      puts "Did not select a #{current_player.color} piece"
+      p start_position
     end
 
-    puts "Select a position to move to:"
-    end_position = current_player.get_position
-    p end_position
+    loop do
+      puts "Select a position to move to:"
+      end_position = current_player.get_position
+
+      begin
+        board.move_piece(start_position, end_position)
+      rescue InvalidMoveError => e
+        puts e.message
+      
+      end
+    end
   end
 end
