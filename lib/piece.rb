cafc780ef3379028c3ccf5ab20e04508ee856e15
Regi Fronda <regisfronda@gmail.com>
2021-10-26 03:35:19 -0700

    Add safe_moves method

--- lib/piece.rb
+++ lib/piece.rb
@@ -8,6 +8,18 @@ class Piece
     @color = color
   end
 
+  def safe_moves
+    moves = []
+    available_moves.each do |move|
+      new_board = board.dup
+      new_board.move_piece!(location, move)
+      if !new_board.in_check?(color)
+        moves << move
+      end
+    end
+    moves
+  end
+
   def enemy?(location)
     !board[location].nil? && board[location].color != color
   end
