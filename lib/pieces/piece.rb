62900035c49bf3bb9a115c8d6c0b2d3959ee43ba
Regi Fronda <regisfronda@gmail.com>
2021-11-01 22:54:26 -0700

    Move piece.rb to pieces directory from lib directory

---
+++ lib/pieces/piece.rb
@@ -0,0 +1,34 @@
+class Piece
+  attr_reader :color, :board
+  attr_accessor :location
+  
+  def initialize(board, location, color)
+    @board = board
+    @location = location
+    @color = color
+  end
+
+  def safe_moves
+    moves = []
+    available_moves.each do |move|
+      new_board = board.dup
+      new_board.move_piece!(location, move)
+      if !new_board.in_check?(color)
+        moves << move
+      end
+    end
+    moves
+  end
+
+  def enemy?(location)
+    !board[location].nil? && board[location].color != color
+  end
+
+  def current_row
+    location.first
+  end
+
+  def current_column
+    location.last
+  end
+end
